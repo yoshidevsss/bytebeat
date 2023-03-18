0e2115c3f18499035e329550a0e729e7b155828a
Chasyxx <105890603+Chasyxx@users.noreply.github.com>
2023-03-18 16:53:19 -0600

    Add other modes
    Add bitbeat, 2048, and logmode

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -164,6 +164,19 @@ class audioProcessor extends AudioWorkletProcessor {
 					return outValue;
 				};
 				break;
+			case 'Bitbeat':
+					this.getValues = (funcValue, ch) =>
+						(this.lastByteValue[ch] = funcValue & 1) - 0.5;
+				break;
+			case '2048':
+					this.getValues = (funcValue, ch) =>
+						(this.lastByteValue[ch] = (funcValue & 2047)/4) / 127.5 - 1;
+			break;
+		case 'logmode':
+				this.getValues = (funcValue, ch) =>
+					(this.lastByteValue[ch] = (Math.log(funcValue)*32)&255) / 127.5 - 1;
+		break;
+
 			default: this.getValues = (funcValue, ch) => (this.lastByteValue[ch] = NaN);
 			}
 		}
