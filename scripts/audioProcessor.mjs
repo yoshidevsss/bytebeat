08e96742e8a1efde54ab6dc716a653aec2f4e74b
Chasyxx <105890603+Chasyxx@users.noreply.github.com>
2023-03-18 17:06:42 -0600

    Update audio system to fix display

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -164,14 +164,21 @@ class audioProcessor extends AudioWorkletProcessor {
 					return outValue;
 				};
 				break;
-			case 'Bitbeat':
-					this.getValues = (funcValue, ch) =>
-						(this.lastByteValue[ch] = funcValue & 1) - 0.5;
-				break;
-			case '2048':
-					this.getValues = (funcValue, ch) =>
-						(this.lastByteValue[ch] = (funcValue & 2047)/4) / 127.5 - 1;
-			break;
+				case 'Bitbeat':
+					this.getValues = (funcValue, ch) => {
+						const outValue = (funcValue&1)-0.5;
+						this.lastByteValue[ch] = funcValue&1?192:64;
+						return outValue;
+					};
+					break;
+					case '2048':
+						this.getValues = (funcValue, ch) => {
+							const temp = funcValue&2047
+							const outValue = (temp/1024)-1;
+							this.lastByteValue[ch] = funcValue&1?192:64;
+							return outValue;
+						};
+						break;
 		case 'logmode':
 				this.getValues = (funcValue, ch) =>
 					(this.lastByteValue[ch] = (Math.log(funcValue)*32)&255) / 127.5 - 1;
