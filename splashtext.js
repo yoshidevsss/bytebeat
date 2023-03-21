3d1e18b50adb43b0e909bdded7e4b81cbf5849a5
Chasyxx <105890603+Chasyxx@users.noreply.github.com>
2023-03-21 16:25:36 -0600

    Update splashtext.js

--- splashtext.js
+++ splashtext.js
@@ -1,7 +1,18 @@
-let splashes = [
-	`What do you mean "this.func is not a function"!?!?`,
-	`[object Object]`,
-	`Bit reverse? Line break!`,
-	`Line break? Bit reverse!`]
-splash = document.getElementById(`splash`)
-splash.innerText= splashes[Math.round((Math.random())*splashes.length)]
+const splashes = [
+`What do you mean "this.func is not a function"!?!?`,
+`[object Object]`,
+`Bit reverse? Line break!`,
+`Line break? Bit reverse!`,
+`Imagine if the website completely broke :skull:`,
+`Go away, logmode10!`,
+`Go away, Sinmode!`,
+`Go away, SinFreq!`,
+`chasyxx.guthub.io/minibaker`,
+`0x8F <3`,
+`Fun fact! If you scroll down, the website looks just like dollChan! But it isn't, it says that right above me.`,
+`missingNo.`,
+`No, i'm not adding Pale Moon compatibillty, juan!`]
+
+const splash = document.getElementById(`splash`)
+const rnd = Math.floor((Math.random())*splashes.length)
+splash.innerText= splashes[rnd]
