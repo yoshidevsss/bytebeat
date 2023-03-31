085c852b2f5f03a578749f3580402da835f05e82
Chasyxx <105890603+Chasyxx@users.noreply.github.com>
2023-03-31 17:41:18 -0600

    attempt fix 1

--- menus.js
+++ menus.js
@@ -11,15 +11,23 @@ globalThis.MAT = new class { //Menus and transformations
 	}
 	commaFormat(){
 		var code = document.getElementById('editor-default')
-		var temp = null
+		var temp = null;
+		var global = false
+		try { //global testing
+		var toencode = bytebeat.editorValue
+			global = true
+		} catch(err) { //local testing
+			console.warn(`Locally testing because ${err.message}`)
+			var toencode = code.value
+		}
 		var inString = false;
 		var stringCount = 0;
-			for (var i=0;i<code.value.length;i++) {
-				switch(code.value[i]){
+			for (var i=0;i<toencode.length;i++) {
+				switch(toencode[i]){
 					case `,`: case `;`:
-						if(!inString && code.value[i+1] != `\n`) {
-							temp = code.value.slice(0,i) + `${code.value[i]}\n` + code.value.slice(i+1,code.value.length)
-							code.value = temp
+						if(!inString && toencode[i+1] != `\n`) {
+							temp = toencode.slice(0,i) + `${toencode[i]}\n` + toencode.slice(i+1,toencode.length)
+							toencode = temp
 						}
 					break
 
@@ -32,5 +40,6 @@ globalThis.MAT = new class { //Menus and transformations
 			if(stringCount&1){
 				console.error("Error in comma-formatting: Unterminated string")
 			}
+			code.value = toencode
 		}
 }
